44703cede94d2d0efbec338e53413c2a76479146
Josh Wintraub <joshbw4@gmail.com>
2021-06-07 17:49:03 -0400

    Added comment for Bootstraping Rails 5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root 'pages#home'
+  root 'pages#home' # Commenting out this line will lead to default Rails app homepage
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
